44eb551467a02cfe6faed9a25bf1ddde69adaa41
sangxnguyen <sangxnguyen@kms-technology.com>
2022-02-22 13:21:19 +0700

    fix: clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  namespace :app do
-    namespace :controllers do
-      namespace :api do
-        namespace :v1 do
-          get 'test/index'
-          get 'test/show'
-        end
-      end
+  namespace :api do
+    namespace :v1 do
+      get 'products/index'
+      get 'products/show'
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
